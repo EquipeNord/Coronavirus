39ce9734543cc71d46352754246ed0a849b3e6ee
JMainard <mainard17@outlook.fr>
2020-04-01 09:46:55 +0200

    Misea joue du mea et du .base pas du .data

--- database/coronappli_base.sql
+++ database/coronappli_base.sql
@@ -1,14 +1,13 @@
+CREATE TABLE `humain`(`humId` INTEGER,`humPseudo` VARCHAR(50),`humRue` VARCHAR(50),`humVille` VARCHAR(50),`humPays` VARCHAR(50),`humCP` VARCHAR(12),`humEmail` VARCHAR(70),`humTelephone` VARCHAR(15),`humMDP` VARCHAR(500),`humLatitude` VARCHAR(100),`humLongitude` VARCHAR(100),primary key(`humId`));
 
-CREATE TABLE `humain`(`humId` INTEGER,`humPseudo` VARCHAR(50),`humRue` VARCHAR(50),`humVille` VARCHAR(50),`humPays` VARCHAR(50),`humCP` VARCHAR(5),`humEmail` VARCHAR(70),`humTelephone` VARCHAR(10),`humMDP` VARCHAR(500),primary key(`humId`));
+CREATE TABLE `symptome`(`symptId` INTEGER,`symptLibelle` VARCHAR(50),`symptGravite` INTEGER,primary key(`symptId`));
 
-CREATE TABLE `date`(`date` DATE,primary key(`date`));
 
-CREATE TABLE `etat`(`etatId` INTEGER,`etatLibelle` VARCHAR(25),`date` DATE NOT NULL, foreign key (`date`) references date(`date`),primary key(`date`,etatId));
+CREATE TABLE `avoir`(`symptId` INTEGER NOT NULL,`humId` INTEGER NOT NULL,`date` DATE NOT NULL, foreign key (`symptId`) references symptome(`symptId`), foreign key (`humId`) references humain(`humId`),primary key(`symptId`,`humId`,`date`));
 
-CREATE TABLE `aide`(`aideId` INTEGER,`aideEffectue` BOOL,`aideDate` DATE,`aideDescription` VARCHAR(500),`humId` INTEGER NOT NULL, foreign key (`humId`) references humain(`humId`),primary key(`aideId`));
+CREATE TABLE `etat`(`etatId` INTEGER,`etatLibelle` VARCHAR(25),primary key(`etatId`));
 
-CREATE TABLE `stade`(`date` DATE NOT NULL,`etatId` INTEGER NOT NULL,`humId` INTEGER NOT NULL, foreign key (`date`,etatId) references etat(`date`,etatId), foreign key (`humId`) references humain(`humId`),primary key(`date`,etatId,`humId`));
+CREATE TABLE `aide`(`aideId` INTEGER,`aideEffectue` BOOL,`aideDate` DATETIME,`aideDescription` VARCHAR(500),`aideLibelle` VARCHAR(50),`humId` INTEGER NOT NULL, foreign key (`humId`) references humain(`humId`),primary key(`aideId`));
 
-CREATE TABLE `symptome`(`symptId` INTEGER,`symptLibelle` VARCHAR(50),`symptGravite` INTEGER,`date` DATE NOT NULL, foreign key (`date`) references date(`date`),primary key(`date`,symptId));
+CREATE TABLE `stade`(`etatId` INTEGER NOT NULL,`humId` INTEGER NOT NULL,`date` DATE NOT NULL, foreign key (`etatId`) references etat(`etatId`), foreign key (`humId`) references humain(`humId`), primary key(`etatId`,`humId`,`date`));
 
-CREATE TABLE `avoir`(`date` DATE NOT NULL,`symptId` INTEGER NOT NULL,`humId` INTEGER NOT NULL, foreign key (`date`,symptId) references symptome(`date`,symptId), foreign key (`humId`) references humain(`humId`),primary key(`date`,symptId,`humId`));
---
+++ database/coronappli_data.sql
@@ -0,0 +1,33 @@
+-- Jeux d'essai pour l'appli coronAppli
+-- Créer le 19-03-2020 par J.Mainard
+
+insert into humain values (2,'Jerem','13 Rue Carnot','Gap','France','05000','mainard17@outlook.fr','0783676638','Test','',''),
+(3,'Nicoco','27 Rue Chicago','Gap','France','05000','granier.nicolas05@gmail.com','0683547896','MDP','',''),
+(4,'Valoche','34 Rue Jacqueline Auriol','Vendôme','France','411000','valdu41@gmail.com','0632313435','MDP','',''),
+(5,'Elisavet','22 Nileos','Athens','Greece','118 51','elisavet@gmail.com','+30684745236','PWD','','');
+
+insert into symptome values (1,'Toux',4),
+(2,'Problème respiratoire',8),
+(3,'Fièvre',2);
+
+insert into avoir values (1,1,'2020-03-10'),
+(2,1,'2020-03-11'),
+(2,2,'2020-03-15');
+
+insert into etat values (1,'Infecte'),
+(2,'Suspect'),
+(3,'Gueri');
+
+insert into aide values (1,0,'2020-03-10','Faire mes course car je suis en fauteuil','Course',1),
+(2,0,'2020-03-10','Faire mes course car je suis en fauteuil','Course',2),
+(3,0,'2020-03-10','Faire mes course car je suis en fauteuil','Course',3); 
+
+
+insert into stade values (1,4,'2020-03-12'),
+(2,4,'2020-03-15'),
+(3,4,'2020-03-20'),
+(1,1,'2020-03-10'),
+(1,2,'2020-03-10');
+
+
+
